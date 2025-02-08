4590ca323fe0b5565002c3f848b8a334458949ee
philip <pbergman@live.nl>
2025-02-08 13:35:31 +0100

    fixed parameter

--- lovelace_extend/generator_extension.py
+++ lovelace_extend/generator_extension.py
@@ -5,7 +5,7 @@ from .abstract_extension import Extension
 
 class GeneratorExtension(Extension, ABC):
     @abstractmethod
-    def get_config(self, force: bool = False) -> Dict[str, Any]:
+    def get_config(self) -> Dict[str, Any]:
         """
         config registering dashboard, see DASHBOARD_BASE_CREATE_FIELDS
         in homeassistant.components.lovelace.const
@@ -18,7 +18,7 @@ class GeneratorExtension(Extension, ABC):
         """
 
     @abstractmethod
-    async def async_generate(self) -> Dict[str, Any]:
+    async def async_generate(self, force: bool = False) -> Dict[str, Any]:
         """
         Build config dict.
         """
